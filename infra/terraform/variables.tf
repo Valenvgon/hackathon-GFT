95227c265f18279b47cf494623089c7085f9b7f8
Valenvgon <vvercherg@gmail.com>
2025-06-28 10:49:22 +0200

    cambios_enfoque_proyecto

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "project" {
+variable "project_id" {
     description = "The GCP project ID where resources will be created"
     type        = string
   
@@ -10,11 +10,3 @@ variable "region" {
  
 }
   
-variables "wif_pool_name" {
-    description = "nombre de la pool de identidad de trabajo (WIF) para Github Actions"
-    type = string 
-}
-
-variables "github_repo" {
-    description = "nombre del repositorio de Github"
-    type = string
